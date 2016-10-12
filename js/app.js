6b7d65f754134857dd2a2c5894aee9b710c34842
松本 和馬 <hello@mtmtkzm.com>
2016-10-13 00:39:15 +0900

    打鍵済み（正答）の文字は灰色に設定

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
-// app.js
-
 // 仮の単語リスト（適当）
 var wordList = ['value', 'version', 'export', 'import', 'input', 'document', 'variable', 'content', 'division', 'markup', 'math', 'length', 'header', 'language', 'encode', 'decode', 'settings', 'stylesheet', 'console', 'error', 'handling', 'class', 'native', 'sudo', 'keyboard', 'pointer', 'format', 'query', 'queue', 'selector'];
+
+// TARGET DOM
 var wordArea = document.querySelector('#js-word');
 
-//出題中の単語
-var nowWord = 'programming';
-var nowCorrect = 0;
+document.addEventListener('keydown', function() {
+  checkType(event.key);
+});
+
+var nowWord = 'programming'; // 現在出題中の単語
+var nowCorrect = 0; // 現在の正答文字数（現在押すべきキーに相当する）
 
 function checkType(downedkey) {
   if ( downedkey == nowWord.charAt(nowCorrect) ) {
-    nowCorrect++;
-    console.log('正解！', downedkey);
+    paintColor();
     if (nowWord.length == nowCorrect) {
       changeWord();
     }
@@ -20,9 +22,25 @@ function checkType(downedkey) {
   }
 }
 
-document.addEventListener('keydown', keydown);
-function keydown(event) {
-  checkType(event.key);
+// 打鍵済みのキーに色を塗る！
+function paintColor () {
+  nowCorrect++; // 正答文字数のカウントUP
+
+  // 既打鍵のDOMを生成
+  var touchedLetter = document.createElement('span');
+  touchedLetter.className = 'touched-letter';
+  touchedLetter.textContent = nowWord.substr(0,nowCorrect);
+  // 未打鍵のDOMを生成
+  var untouchedLetter = document.createElement('span');
+  untouchedLetter.className = 'untouched-letter';
+  untouchedLetter.textContent = nowWord.substr(nowCorrect, nowWord.length);
+
+  // 一旦、空に
+  wordArea.textContent = null;
+
+  // 打鍵を更新
+  wordArea.appendChild(touchedLetter);
+  wordArea.appendChild(untouchedLetter);
 }
 
 // 出題単語を変更する処理
