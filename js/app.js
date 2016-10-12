d3b0ba2a33fc21d3a7f98f203d43c47efe8c6d4f
松本 和馬 <hello@mtmtkzm.com>
2016-10-12 23:30:18 +0900

    単語の変更処理と押下キーの取得

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 // app.js
 
 // 仮の単語リスト（適当）
-var wordList = ['value','version','E-commerce','Replace','Nest','Bracket','Admin','External','export','import','input','document','variable','content','division','markup','math','length','header','language','encode','decode','settings','stylesheet','console','error','handling','class','native','sudo','keyboard','pointer','format','query','queue','selector'];
-
+var wordList = ['value', 'version', 'E-commerce', 'Replace', 'Nest', 'Bracket', 'Admin', 'External', 'export', 'import', 'input', 'document', 'variable', 'content', 'division', 'markup', 'math', 'length', 'header', 'language', 'encode', 'decode', 'settings', 'stylesheet', 'console', 'error', 'handling', 'class', 'native', 'sudo', 'keyboard', 'pointer', 'format', 'query', 'queue', 'selector'];
 var wordArea = document.querySelector('#js-word');
 
-var word = Math.floor(Math.random()*wordList.length);
-wordArea.textContent = wordList[word];
+//出題中の単語
+var nowWord = '';
 
+// 出題単語を変更する処理
+function changeWord() {
+  var word = Math.floor(Math.random() * wordList.length);
+  wordArea.textContent = nowWord = wordList[word];
+}
+
+document.addEventListener('keydown', keydown);
+function keydown(event) {
+  console.log(event.key);
+}
