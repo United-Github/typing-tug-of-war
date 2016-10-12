8b50cfbacb059d573bea7f762fa36d0032887ac1
松本 和馬 <hello@mtmtkzm.com>
2016-10-12 22:58:30 +0900

    仮の単語リストを自前で適当につくってランダムで表示

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // app.js
-console.log('app.js loaded!');
+
+// 仮の単語リスト
+var wordList = ['value','E-commerce','Replace','Nest','Bracket','Admin','External','export','import','input','document','variable','content','division','markup','math','length','header','language','encode','decode','settings','stylesheet','console','error','handling','class','native']
+
+var wordArea = document.querySelector('#js-word');
+
+var word = Math.floor(Math.random()*wordList.length);
+wordArea.textContent = wordList[word];
+
