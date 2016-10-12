612cf63635f0a2806c2d3341e0f7baa272775a6f
松本 和馬 <hello@mtmtkzm.com>
2016-10-12 23:01:22 +0900

    ちょっと増やした

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // app.js
 
-// 仮の単語リスト
-var wordList = ['value','E-commerce','Replace','Nest','Bracket','Admin','External','export','import','input','document','variable','content','division','markup','math','length','header','language','encode','decode','settings','stylesheet','console','error','handling','class','native']
+// 仮の単語リスト（適当）
+var wordList = ['value','version','E-commerce','Replace','Nest','Bracket','Admin','External','export','import','input','document','variable','content','division','markup','math','length','header','language','encode','decode','settings','stylesheet','console','error','handling','class','native','sudo','keyboard','pointer','format','query','queue','selector'];
 
 var wordArea = document.querySelector('#js-word');
 
