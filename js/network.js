37da9d0d72b1e55e87a5a9ab6d41595f3a523899
ragrog <ragrog216@outlook.jp>
2016-10-15 17:03:30 +0900

    認証を切っておく

--- js/network.js
+++ js/network.js
@@ -1,1 +1,15 @@
-console.log('networkだよ');
+var main = function(){
+	// // Initialize Firebase
+	// var config = {
+	// 	apiKey: "AIzaSyB4SrBOJXjb96oQHhufas5bU01AqJcZ6oQ",
+	// 	authDomain: "tug-of-war-fc1a3.firebaseapp.com",
+	// 	databaseURL: "https://tug-of-war-fc1a3.firebaseio.com",
+	// 	storageBucket: "",
+	// 	messagingSenderId: "69512977389"
+	// };
+	// firebase.initializeApp(config);   
+	// console.log('初期化したよ');
+	// var hoge = '1';
+}
+
+main();
